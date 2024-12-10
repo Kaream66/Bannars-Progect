5c7746b00428ecab0596dce3f0ce9337003c6c52
Kaream <kareamtanany@gmail.com>
2024-12-10 14:21:03 +0200

    create add ,delete, edit ecklirik pages

--- lib/pages/eckrelick/delete_eckrelick.dart
+++ lib/pages/eckrelick/delete_eckrelick.dart
@@ -1,9 +1,63 @@
+import 'package:banners_app/widgets/choose_widget.dart';
+import 'package:banners_app/widgets/custom_button.dart';
+import 'package:banners_app/widgets/image_widget.dart';
 import 'package:flutter/material.dart';
 class DeleteEckrelick extends StatelessWidget {
   const DeleteEckrelick({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return  Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              //crossAxisAlignment: CrossAxisAlignment.,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0, right: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        '  حذف  اكريليك',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Image.asset('assets/images/Backarrow.jpg'),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const ImageWidget(imagePath: 'assets/images/ecklerik.jpg'),
+                const SizedBox(
+                  height: 35,
+                ),
+                const ChooseWidget(chooseName: 'أختر اللون'),
+                const SizedBox(
+                  height: 180,
+                ),
+                CustomButton(label: 'حذف', onTap: () {}),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
--- lib/pages/eckrelick/edit_eckrelick.dart
+++ lib/pages/eckrelick/edit_eckrelick.dart
@@ -1,9 +1,77 @@
+import 'package:banners_app/widgets/add_widget.dart';
+import 'package:banners_app/widgets/choose_widget.dart';
+import 'package:banners_app/widgets/custom_button.dart';
+import 'package:banners_app/widgets/image_widget.dart';
 import 'package:flutter/material.dart';
 class EditEckrelick extends StatelessWidget {
   const EditEckrelick({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView(children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              //crossAxisAlignment: CrossAxisAlignment.,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(top: 30.0, right: 30),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        '  تعديل  اكريليك',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Image.asset('assets/images/Backarrow.jpg'),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const ImageWidget(imagePath: 'assets/images/ecklerik.jpg'),
+                const SizedBox(
+                  height: 35,
+                ),
+                const ChooseWidget(chooseName: 'أختر اللون'),
+                const SizedBox(
+                  height: 20,
+                ),
+                const ChooseWidget(chooseName: 'أختر اللون'),
+                const SizedBox(
+                  height: 15,
+                ),
+               
+                const AddWidget(
+                  text: 'تعديل المقاس',
+                  label: 'تعديل المقاس',
+                ),
+                const SizedBox(
+                  height: 90,
+                ),
+                CustomButton(label: 'تعديل', onTap: () {}),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
